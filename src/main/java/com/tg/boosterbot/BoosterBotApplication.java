0b89d1a73427af5f6cce6443786995c95ef4e477
Mikhail <mrus7684@gmail.com>
2026-02-26 18:06:32 +0300

    Структура проекта, 1-й коммит

---
+++ src/main/java/com/tg/boosterbot/BoosterBotApplication.java
@@ -0,0 +1,13 @@
+package com.tg.boosterbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoosterBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoosterBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tg/boosterbot/BoosterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tg.boosterbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoosterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
